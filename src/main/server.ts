164c57ea7a9f1b003d0bf5bfcecfdf41be0aeee7
Marcos Guilherme <marcos.sassaki@grupointegrado.br>
2023-08-29 15:22:55 -0300

    setup

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,6 +3,7 @@ import router from "../modules/pessoa/routes";
 
 const app = express();
 
+app.use(express.json());
 app.use('/pessoa', router);
 
 app.listen(3000, () => console.log("rodando na 3000"));
--- src/modules/pessoa/controller.ts
+++ src/modules/pessoa/controller.ts
@@ -4,13 +4,13 @@ import { InsertPessoa, pessoa } from "./model";
 
 export async function createPessoaController (request: Request, response: Response) {
     try {
-        console.log("a")
+
         const newPessoa: InsertPessoa = request.body;
 
         const createdPessoa = await db.insert(pessoa).values(newPessoa)
 
-        return response.sendStatus(200).json({ createdPessoa })
+        return response.sendStatus(200).json( createdPessoa )
     } catch(error) {
-        response.sendStatus(500).json(error)
+        throw new Error(error)
     }
 }
