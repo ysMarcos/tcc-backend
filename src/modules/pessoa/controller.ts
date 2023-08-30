8c32bff1b38fd137cfe6390f5f6014c3c7773f84
ysMarcos <marguilherme02@gmail.com>
2023-08-29 22:32:51 -0300

    fix: express funcionando

--- src/modules/pessoa/controller.ts
+++ src/modules/pessoa/controller.ts
@@ -1,16 +1,18 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { db } from "../../db";
 import { InsertPessoa, pessoa } from "./model";
 
-export async function createPessoaController (request: Request, response: Response) {
+export async function createPessoaController (request: Request, response: Response, next: NextFunction) {
     try {
 
         const newPessoa: InsertPessoa = request.body;
 
         const createdPessoa = await db.insert(pessoa).values(newPessoa)
+        console.log(createdPessoa)
 
-        return response.sendStatus(200).json( createdPessoa )
+        response.status(200)
+            .json( createdPessoa )
     } catch(error) {
-        throw new Error(error)
+        next(error)
     }
 }
