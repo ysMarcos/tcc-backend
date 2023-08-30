c62f8f2eafaa9e34a9d7a920fd02f8e9dbbf02d0
Marcos Guilherme <marcos.sassaki@grupointegrado.br>
2023-08-30 09:35:55 -0300

    feat: PessoaController terminado

--- src/modules/pessoa/controller.ts
+++ src/modules/pessoa/controller.ts
@@ -1,18 +1,65 @@
 import { NextFunction, Request, Response } from "express";
 import { db } from "../../db";
-import { InsertPessoa, pessoa } from "./model";
+import { InsertPessoa, Pessoa, pessoa } from "./model";
+import { eq } from 'drizzle-orm';
 
 export async function createPessoaController (request: Request, response: Response, next: NextFunction) {
     try {
-
         const newPessoa: InsertPessoa = request.body;
 
-        const createdPessoa = await db.insert(pessoa).values(newPessoa)
-        console.log(createdPessoa)
+        const createdPessoa = await db.insert(pessoa).values(newPessoa);
 
         response.status(200)
             .json( createdPessoa )
     } catch(error) {
         next(error)
+    }
+}
+
+export async function listPessoaController (request: Request, response: Response, next: NextFunction) {
+    try{
+        const pessoas: Pessoa[] = await db.select().from(pessoa).orderBy(pessoa.nome);
+
+        response.status(200).json(pessoas);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function getPessoaByIdController (request: Request, response: Response, next: NextFunction) {
+    try {
+        const id  = Number(request.params.id);
+        const selectPessoa: Pessoa[] = await db.select().from(pessoa).where(eq(pessoa.id, id)).limit(1);
+        response.status(200).json(selectPessoa[0]);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function updatePessoaController(request: Request, response: Response, next: NextFunction) {
+    try {
+        const id = Number(request.params.id);
+        const data = request.body;
+
+        const pessoaToUpdate = await db.select({
+            id: pessoa.id
+        }).from(pessoa).where(eq(pessoa.id, id)).limit(1);
+        const updatePessoa = await db.update(pessoa).set({
+            ...pessoaToUpdate[0],
+            ...data
+        }).where(eq(pessoa.id, id));
+        response.status(200).json(updatePessoa)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function deletePessoaController(request: Request, response: Response, next: NextFunction) {
+    try {
+        const id = Number(request.params.id);
+        const deletedPessoa = await db.delete(pessoa).where(eq(pessoa.id, id));
+        return response.status(200).json(deletedPessoa);
+    } catch (error){
+        next(error);
     }
 }
--- src/modules/pessoa/routes.ts
+++ src/modules/pessoa/routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { createPessoaController } from "./controller";
+import { createPessoaController, deletePessoaController, getPessoaByIdController, listPessoaController, updatePessoaController } from "./controller";
 
 const router = express.Router();
 
 router.post('/new', createPessoaController);
+router.get('/list', listPessoaController);
+router.get('/get/:id', getPessoaByIdController);
+router.put('/update/:id', updatePessoaController);
+router.delete('/delete/:id', deletePessoaController);
 
 export default router;
