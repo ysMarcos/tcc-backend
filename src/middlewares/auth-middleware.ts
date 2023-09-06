87e99b0a1bc8328f0661e9a32b1f2a6b9e2391f5
Marcos Guilherme <marcos.sassaki@grupointegrado.br>
2023-09-06 14:39:33 -0300

    feat: adicionado interface do token

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -2,6 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { jwtSecret } from "../../env";
 
+interface TokenPayload {
+    id: number,
+    usuario: string,
+    dataInicio: Date,
+    dataPrevisaoFim: Date,
+    ativo: boolean,
+    pessoaId: number,
+    iat: number,
+    exp: number
+}
+
 export async function ensureAuthenticated (request: Request, response: Response, next: NextFunction) {
     const { authorization } = request.headers;
     if(!authorization) return response.sendStatus(401);
@@ -10,7 +21,10 @@ export async function ensureAuthenticated (request: Request, response: Response,
 
     try{
         const data = jwt.verify(token, jwtSecret);
-        next();
+        const { id } = data as TokenPayload;
+
+        request.userId = id;
+        return next();
     } catch {
         return response.sendStatus(401);
     }
