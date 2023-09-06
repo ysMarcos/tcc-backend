e07c9452558445a9a5e877ceaf6d9c77c5a0291e
Marcos Guilherme <marcos.sassaki@grupointegrado.br>
2023-09-06 09:19:05 -0300

    fix: authMiddleware agora retorna status 401

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -4,15 +4,14 @@ import { jwtSecret } from "../../env";
 
 export async function ensureAuthenticated (request: Request, response: Response, next: NextFunction) {
     const { authorization } = request.headers;
-    if(!authorization) return response.status(401);
+    if(!authorization) return response.sendStatus(401);
 
     const token = authorization.replace('Bearer', '').trim();
 
     try{
         const data = jwt.verify(token, jwtSecret);
-        console.log(data)
         next();
     } catch {
-        return response.status(401);
+        return response.sendStatus(401);
     }
 }
