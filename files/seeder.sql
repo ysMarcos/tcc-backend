05c99710dabc8417a5c04b802ff80cb48e6c08ad
Marcos Guilherme <marcos.sassaki@grupointegrado.br>
2023-09-19 15:04:35 -0300

    feat: seeder sql

---
+++ files/seeder.sql
@@ -0,0 +1,31 @@
+select * from `unidade-medida`;
+insert into `unidade-medida` values (null, "kg", sysdate());
+
+insert into item
+values
+(null, "aaa", 525, 1, sysdate()),
+(null, "baa", 5236, 1, sysdate()),
+(null, "caa", 234, 1, sysdate()),
+(null, "daa", 523, 1, sysdate()),
+(null, "eaa", 124, 1, sysdate()),
+(null, "faa", 2, 1, sysdate()),
+(null, "gaa", 6265, 1, sysdate()),
+(null, "haa", 2369, 1, sysdate()),
+(null, "iaa", 838, 1, sysdate()),
+(null, "jaa", 569, 1, sysdate()),
+(null, "kaa", 1, 1, sysdate()),
+(null, "laa", 74, 1, sysdate()),
+(null, "maa", 20, 1, sysdate()),
+(null, "naa", 378, 1, sysdate()),
+(null, "oaa", 200, 1, sysdate()),
+(null, "paa", 1258, 1, sysdate()),
+(null, "qaa", 5.5, 1, sysdate()),
+(null, "raa", 1245, 1, sysdate()),
+(null, "saa", 24, 1, sysdate()),
+(null, "taa", 982, 1, sysdate()),
+(null, "uaa", 90, 1, sysdate()),
+(null, "vaa", 100, 1, sysdate()),
+(null, "waa", 10000, 1, sysdate()),
+(null, "xaa", 52, 1, sysdate()),
+(null, "yaa", 29, 1, sysdate()),
+(null, "zaa", 5, 1, sysdate())
