6eaf71874cbf6f7bf451b1e631fe643eaf4dd54f
Marcos Guilherme <marcos.sassaki@grupointegrado.br>
2023-09-20 15:15:13 -0300

    refactor: refeito update item controller

--- src/modules/item/routes.ts
+++ src/modules/item/routes.ts
@@ -4,15 +4,17 @@ import { verifyPermission } from "../../middlewares/permission-middleware.js";
 import { createItem, deleteItem, getItemById, listItem, updateItem } from "./controller.js";
 import { insertItemTest } from "./controllers/insert-item.js";
 import { listItemTest } from "./controllers/list-item.js";
+import { updateItemTest } from "./controllers/update-item.js";
 
 const router = express.Router();
 
 router.post('/new', createItem);
-router.post('/new/test', ensureAuthenticated, verifyPermission(['admin','insert-item']), insertItemTest);
+router.post('/new/test', ensureAuthenticated, verifyPermission(['admin','create-item']), insertItemTest);
 router.get('/get/:id', getItemById);
 router.get('/list', ensureAuthenticated, verifyPermission(['admin','get-item']), listItem);
 router.get('/list/test', listItemTest);
 router.put('/update/:id', updateItem);
+router.put('/update/:id/test', ensureAuthenticated, verifyPermission(['admin', 'update-item']), updateItemTest);
 router.delete('/delete/:id', deleteItem);
 
 export default router;
