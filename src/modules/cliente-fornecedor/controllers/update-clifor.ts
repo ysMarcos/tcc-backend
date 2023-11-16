b4b53cdb336bcfac71acbb2381b94bc6175f0026
ysMarcos <marguilherme02@gmail.com>
2023-11-16 10:49:40 -0300

    fix: update cliente

--- src/modules/cliente-fornecedor/controllers/update-clifor.ts
+++ src/modules/cliente-fornecedor/controllers/update-clifor.ts
@@ -2,17 +2,47 @@ import { eq, sql } from 'drizzle-orm';
 import { Request, Response } from 'express';
 import { db } from '../../../db';
 import { clienteFornecedorTable } from '../schema';
+import { pessoaTable } from '../../pessoa/schema';
+
+type newData = {
+    nome: string,
+    email: string,
+    telefone: string,
+    cadastro: string,
+    registro: string
+}
 
 export async function updateClienteFornecedor(request: Request, response: Response){
-    const newData = request.body;
     const id = Number(request.params.id);
 
+    const requestData: newData = request.body;
+
+    let newData: Record<string, string> = {};
+    if(requestData.nome && requestData.nome.length >= 3) newData.nome = requestData.nome;
+    if(requestData.email && requestData.email.length >= 3) newData.email = requestData.email;
+    if(requestData.telefone && requestData.telefone.length >= 3) newData.telefone = requestData.telefone;
+    if(requestData.cadastro && requestData.cadastro.length >= 3) newData.cadastro = requestData.cadastro;
+    if(requestData.registro && requestData.registro.length >= 3) newData.registro = requestData.registro;
+
     const sqlQuery = db
-        .select()
-        .from(clienteFornecedorTable)
+        .select({
+            id: pessoaTable.id,
+            nome: pessoaTable.nome,
+            email: pessoaTable.email,
+            telefone: pessoaTable.telefone,
+            cadastro: pessoaTable.cadastro,
+            registro: pessoaTable.registro
+        })
+        .from(pessoaTable)
+        .innerJoin(
+            clienteFornecedorTable,
+            eq(
+                clienteFornecedorTable.pessoaId, pessoaTable.id
+            )
+        )
         .where(
             eq(
-                clienteFornecedorTable.id, sql.placeholder("id")
+                pessoaTable.id, sql.placeholder("id")
             )
         )
         .prepare();
@@ -22,16 +52,16 @@ export async function updateClienteFornecedor(request: Request, response: Respon
             const [clifor] = await sqlQuery.execute({ id });
             if(!clifor){
                 transaction.rollback();
-                return;
             }
-            const result = await db
-                .update(clienteFornecedorTable)
+            const result = await transaction
+                .update(pessoaTable)
                 .set({
                     ...clifor,
                     ...newData
                 })
-                .where(eq(
-                    clienteFornecedorTable.id, id
+                .where(
+                eq(
+                    pessoaTable.id, clifor.id
                 ))
             return result;
         })
