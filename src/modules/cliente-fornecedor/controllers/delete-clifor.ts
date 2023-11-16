c121120dae5a53870712cde6d1ed520c9ed49ca4
ysMarcos <marguilherme02@gmail.com>
2023-11-16 11:08:54 -0300

    fix: delete clifor e pessoa

--- src/modules/cliente-fornecedor/controllers/delete-clifor.ts
+++ src/modules/cliente-fornecedor/controllers/delete-clifor.ts
@@ -2,6 +2,7 @@ import { eq, sql } from 'drizzle-orm';
 import { Request, Response } from 'express';
 import { db } from '../../../db';
 import { clienteFornecedorTable } from '../schema';
+import { pessoaTable } from '../../pessoa/schema';
 
 export async function deleteClienteFornecedor(request: Request, response: Response){
     const { params } = request;
@@ -10,12 +11,20 @@ export async function deleteClienteFornecedor(request: Request, response: Respon
         .delete(clienteFornecedorTable)
         .where(
             eq(
-                clienteFornecedorTable.id, sql.placeholder("id")
+                clienteFornecedorTable.pessoaId, sql.placeholder("id")
             )
         )
         .prepare();
     try {
-        const result = await sqlQuery.execute({ id });
+        const result = await db.transaction( async (transaction) => {
+            await sqlQuery.execute({id});
+            const result = await transaction
+                .delete(pessoaTable)
+                .where(eq(
+                    pessoaTable.id, id
+                ))
+            return result
+        })
         return response.status(200).json(result);
     } catch(error){
         return response.status(500).json(error);
