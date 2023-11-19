3e2d66462e372a33f6d679d7f5cf34b13d68bc13
ysMarcos <marguilherme02@gmail.com>
2023-11-19 12:13:54 -0300

    fix: update endereco

--- src/modules/endereco/controllers/update-endereco.ts
+++ src/modules/endereco/controllers/update-endereco.ts
@@ -3,12 +3,30 @@ import { db } from "../../../db";
 import { enderecoTable } from "../schema";
 import { sql, eq } from "drizzle-orm";
 
+
 export async function updateEndereco(request: Request, response: Response) {
     const id = Number(request.params.id);
     const data = request.body;
 
+    let newData: Record<string, string> = {};
+    if(data.rua && data.rua.length >= 3) newData.rua = data.rua;
+    if(data.numero && data.numero.length > 0) newData.numero = data.numero;
+    if(data.bairro && data.bairro.length >=3 ) newData.bairro = data.bairro;
+    if(data.cep && data.cep.length === 8) newData.cep = data.cep;
+    if(data.complemento && data.complemento.length >= 0) newData.complemento = data.complemento;
+    if(data.cidade && data.cidade.length >= 0) newData.cidade = data.cidade;
+    if(data.tipo && data.tipo.length) newData.tipo = data.tipo;
+
     const enderecoSql = db
-        .select()
+        .select({
+            rua: enderecoTable.rua,
+            numero: enderecoTable.numero,
+            bairro: enderecoTable.bairro,
+            cep: enderecoTable.cep,
+            complemento: enderecoTable.complemento,
+            cidade: enderecoTable.cidade,
+            tipo: enderecoTable.tipo
+        })
         .from(enderecoTable)
         .where(
             eq(
@@ -20,13 +38,14 @@ export async function updateEndereco(request: Request, response: Response) {
     try {
 
         const result = await db.transaction(async (transaction) => {
-            const endereco = await enderecoSql.execute({id});
+            const [endereco] = await enderecoSql.execute({id});
+            if(!endereco) transaction.rollback();
 
-            const result = await db
+            const result = await transaction
             .update(enderecoTable)
             .set({
                 ...endereco,
-                ...data
+                ...newData
             })
             .where(
                 eq(enderecoTable.id, id)
